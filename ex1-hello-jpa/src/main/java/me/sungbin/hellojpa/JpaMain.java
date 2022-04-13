4944d235f64229dec4687bdb344b643ad1a9c9f8
SungbinYang <ysb05222@naver.com>
2022-04-13 22:01:47 +0900

    일대다 [1:N]

--- ex1-hello-jpa/src/main/java/me/sungbin/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/me/sungbin/hellojpa/JpaMain.java
@@ -4,7 +4,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.util.List;
 
 /**
  * packageName : me.sungbin.hellojpa
@@ -27,30 +26,17 @@ public class JpaMain {
         transaction.begin();
 
         try {
-            // 저장
-            Team team = new Team();
-            team.setName("TeamA");
-//            team.getMembers().add(member); // 잘못된 연관관계 매핑 :: 주인쪽에 맺어줘야한다.
-            entityManager.persist(team);
-
             Member member = new Member();
             member.setName("member1");
-            member.setTeam(team);
+
             entityManager.persist(member);
 
-            entityManager.flush();
-            entityManager.clear();
+            Team team = new Team();
+            team.setName("teamA");
+            //
+            team.getMembers().add(member);
 
-            team.addMember(member);
-
-            Team findTeam = entityManager.find(Team.class, team.getId()); // 1차 캐시
-            List<Member> members = findTeam.getMembers();
-
-            System.out.println("====================");
-            for (Member m : members) {
-                System.out.println("m.getName() = " + m.getName());
-            }
-            System.out.println("====================");
+            entityManager.persist(team);
 
             transaction.commit(); // 이때 DB에 쿼리를 날린다.
         } catch (Exception e) {
--- ex1-hello-jpa/src/main/java/me/sungbin/hellojpa/Team.java
+++ ex1-hello-jpa/src/main/java/me/sungbin/hellojpa/Team.java
@@ -25,7 +25,8 @@ public class Team {
 
     private String name;
 
-    @OneToMany(mappedBy = "team")
+    @OneToMany
+    @JoinColumn(name = "TEAM_ID")
     private List<Member> members = new ArrayList<>();
 
     public Long getId() {
@@ -50,10 +51,5 @@ public class Team {
 
     public void setMembers(List<Member> members) {
         this.members = members;
-    }
-
-    public void addMember(Member member) {
-        member.setTeam(this);
-        members.add(member);
     }
 }
